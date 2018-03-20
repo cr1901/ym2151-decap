176d824dc64aac3703bf7953fb20aa3dc56f7448
William D. Jones <thor0505@comcast.net>
2018-03-19 20:44:00 -0400

    Start making incremental improvements to blog transistor extraction.

--- sgen/blog.lisp
+++ sgen/blog.lisp
@@ -31,45 +31,66 @@
 ;(set roi (list 2950 8550 3300 8200))
 ;(set roi (list 2986 8450 3040 8290))
 
-(let [blog (nodes_rect 2986 8450 3040 8290)]
-   (let [n (make_match blog)]
-       (let [f (io.open "blog-match.txt" "w+")]
-         (do
-           ;(print n)
-           (f.write f n)
-           (f.close f)))
-       (let [pullups_left (.. "P0=d(v0, a, a)\n"
-                           "P1=d(v1, b, b)\n"
-                           "P2=d(v2, c, c)\n"
-                           "P3=d(v3, d, d)\n"
-                           "P4=d(v4, e, e)\n"
-                           "P5=d(v5, f, f)\n"
-                           "P6=d(v6, g, g)\n"
-                           "P7=d(v7, h, h)\n"
-                           "P8=d(v8, i, i)\n"
-                           "P9=d(v9, j, j)\n"
-                           "V0=vcc(v0)\n"
-                           "V1=vcc(v1)\n"
-                           "V2=vcc(v2)\n"
-                           "V3=vcc(v3)\n"
-                           "V4=vcc(v4)\n"
-                           "V5=vcc(v5)\n"
-                           "V6=vcc(v6)\n"
-                           "V7=vcc(v7)\n"
-                           "V8=vcc(v8)\n"
-                           "V9=vcc(v9)\n")]
+; Get a row of transistors in a ROM plus corresponding pullups.
+; Order of transistors not guaranteed.
+(set row_str
+  (fn [row_n n_trans]
+    (set iter
+      (fn [str curr_n]
+        (if (>= curr_n n_trans)
+          str
+          :else
+          (iter
+            (.. str (mk_str curr_n))
+            (+ 1 curr_n)))))
 
-            (do
-              (let [x_bias 3050
-                    y_bias 0
-                    p_match (match blog pullups_left)]
-                  (do
-                    (move p_match x_bias y_bias
-                      (move_pullup->str 2 0 8441 ""))
-                    (move p_match x_bias y_bias
-                      (p_col 0 0 8424 2 ""))
-                    (move p_match x_bias y_bias
-                      (p_col 3 0 8392 7 ""))
-                    (rename p_match)))))))
+    (set mk_str
+      (fn [curr_n]
+        (let [trans_id (string.format "D%d_%d" row_n curr_n)
+              node_str (string.format "t(p%d, d%d_%d, g%d_%d)"
+                                      row_n row_n curr_n row_n curr_n)
+              gnd_id (string.format "G%d_%d=gnd(g%d_%d)" row_n curr_n row_n curr_n)]
+          (.. trans_id "=" node_str " " gnd_id "\n"))))
+
+    (let [pullup_str (string.format
+                       "P_l%d=d(v%d, p%d, p%d) V%d=vcc(v%d)\n"
+                        row_n row_n row_n row_n row_n row_n)]
+          ; term_str (string.format
+          ;            "T%d=t(p%d, t_%d, gt%d) GT%d=gnd(gt%d)\n \
+          ;            P_b%d=d(vpb%d, t_%d, t_%d) VPb%d=vcc(vpb%d)\n"
+          ;            row_n row_n row_n row_n row_n row_n)]
+      (.. (iter pullup_str 0) ""))))
+
+;(print (row_str 0 4))
+
+(let [blog (nodes_rect 2950 8550 3300 8200)
+      n (make_match blog)]
+     (let [f (io.open "blog-match.txt" "w+")]
+       (do
+         ;(print n)
+         (f.write f n)
+         (f.close f)))
+     (let [pullups_left  (.. (row_str 0 5) (row_str 1 5))]
+
+          (do
+            (print pullups_left)
+            (let [x_bias 3050
+                  y_bias 0
+                  p_match (match blog pullups_left)]
+                (do
+                  (print p_match)
+                  ; (move p_match x_bias y_bias
+                  ;   (move_pullup->str 2 0 8441 ""))
+                  ; (move p_match x_bias y_bias
+                  ;   (p_col 0 0 8424 2 ""))
+                  ; (move p_match x_bias y_bias
+                  ;   (p_col 3 0 8392 7 ""))
+                  (rename p_match))))))
+
+
+; (let [row0 (nodes_rect 3040 8450 3200 8435)]
+;   (let [p_match (make_match row0)]
+;     ;(match row0 ())
+;     (print p_match)))
 
 ;(let [blog (nodes_rect 2950 8550 3300 8200)] (let [row_]))
